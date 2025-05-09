b2d513c0da344adf7260d4516d91924721b8a5b4
den zak <den.zak776@gmail.com>
2025-05-09 21:14:51 +0300

    add initial files

---
+++ js/task-3.js
@@ -0,0 +1,9 @@
+function getElementWidth(content, padding, border) {
+    const contentValue = parseFloat(content);
+    const paddingValue = parseFloat(padding);
+    const borderValue = parseFloat(border);
+  
+    const totalWidth = contentValue + 2 * paddingValue + 2 * borderValue;
+  
+    return totalWidth;
+  }
